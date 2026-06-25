660944a834c6783371fd43ab32a00e67bae17a5b
santosh1024 <rawatsantosh2005@gmail.com>
2026-06-26 04:28:46 +0530

    Add production start script

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/IMS";
-
+const mongoURI = "mongodb+srv://rawatsantosh2005_db_user:Jv06S0Ijnuw2bBQS@cluster0.9msfl2w.mongodb.net/IMS?retryWrites=true&w=majority&appName=Cluster0";
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
